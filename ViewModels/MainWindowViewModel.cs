9909ce95b07b364f5eeafc987afbe100e0c15312
Ievlev Yegor <ievlevegor03.taxix@gmail.com>
2025-08-20 12:20:50 +0300

    Добавьте файлы проекта.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EMusic.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace EMusic.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
