9ad818026be7fafef2313ddf25261ab518f774b4
Ievlev Yegor <ievlevegor03.taxix@gmail.com>
2025-08-21 13:07:02 +0300

    Added Models and ViewModels

---
+++ ViewModels/PlayerViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMusic.ViewModels
+{
+    public partial class PlayerViewModel : ViewModelBase
+    {
+    }
+}
